bfb72288678dc18c4209de03c5a220c0a52f7a10
Visalroth <chouvisalroth9999@gmail.com>
2025-08-26 13:18:33 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+def index():
+    return {'data' : {'name': 'Vza'}}
+
+@app.get('/about')
+def about():
+    return {'data' : "about page"}
+
+@app.get('/contact')
+def contact():
+    return {'data' : "my contact page"}
